d95609c3a3534529d8cbf8e1f16aef93a41153b3
Jaime <jai.hernandeza@duocuc.cl>
2025-05-21 14:53:37 -0400

    Inicialización del Proyecto

---
+++ src/main/java/com/gestion/animales/GestionDeAnimalesApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.animales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeAnimalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeAnimalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestion/animales/GestionDeAnimalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.animales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeAnimalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
