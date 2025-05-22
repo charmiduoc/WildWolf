fdf2b52ab17663a0a90ce55ae3e1e5ffacfc1d33
Jaime <jai.hernandeza@duocuc.cl>
2025-05-22 16:58:06 -0400

    Controlador animal incompleto

--- src/main/java/com/gestion/animales/controller/AnimalController.java
+++ src/main/java/com/gestion/animales/controller/AnimalController.java
@@ -3,6 +3,7 @@ package com.gestion.animales.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.gestion.animales.model.Animal;
 import com.gestion.animales.service.AnimalService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 @RestController
 @RequestMapping("/api/v1/animales")
@@ -25,7 +32,31 @@ public class AnimalController {
         }
         return ResponseEntity.ok(animales);
     }
+
+    @PostMapping
+    public ResponseEntity<Animal> guardar(@RequestBody Animal animal){
+        Animal nuevo = animalService.save(animal);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevo);
+    }
     
+    @GetMapping("/{id}")
+    public ResponseEntity<Animal> buscar(@PathVariable Integer id){
+        try{
+            Animal animal = animalService.findById(id);
+            return ResponseEntity.ok(animal);
+        }
+        catch(Exception e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Animal> actualizar(@PathVariable Integer id, @RequestBody Animal animal){
+        try{
+            Animal ani = animalService.findById(id);
+            ani.setId(id);
+        }
+    }
 }
 
 /*
